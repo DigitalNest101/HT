b664751fcdc0533e019da4c6828f0a2e070b3c09
DigitalNest101 <84118920+DigitalNest101@users.noreply.github.com>
2022-09-26 22:57:07 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,222 @@
+<?php include('./partials/header.php')  ?>
+
+
+  <main id="main" style="margin-top: 106px;">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs d-flex align-items-center" style="background-image: url('assets/img/2.jpg');">
+      <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+
+        <h2>About</h2>
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>About</li>
+        </ol>
+
+      </div>
+    </div>
+    <!-- End Breadcrumbs -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row position-relative">
+
+          <div class="col-lg-7 about-img" style="background-image: url(assets/img/about.jpg);"></div>
+
+          <div class="col-lg-7">
+            <h2>HIGH TENSILE</h2>
+            <div class="our-story">
+              <h4>Est 1988</h4>
+              <h3>Our Story</h3>
+             <p>Our firm was incorporated in the year 2009 as a Partnership entity at Balanagar in Hyderabad. The partner, Mr. Parashuram has excellent entrepreneur skills of running the business. He has remained totally devoted towards all the business activities right from purchase of Raw Materials to fabrication of products and to final finish till delivery. The product range Sheet metal fabrications using the ALUMINUM, CRC, GI, SS SHEETS. latest technology with well equipped infrastructure for any type of manufacturing. ELECTRONIC & ELECTRICAL ENCLOSURE, FIRE BOXES, PRESSED STEEL DOORS WINDOW FRAMES. Factory Establishd in The 2009-made under strict supervision in our fully equipped industrial unit situated at Hydarabad.. The firm has employed technical professionals to ensure effective steps have taken to maintain the quality at all stages of production and to take care of perfection of products fabricated for the full satisfaction of the clients. The competence of our personnel is backed up by many years of experience in the profession.</p>
+            
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <!-- End About Section -->
+
+   
+
+    <!-- ======= Alt Services Section ======= -->
+    <section id="alt-services" class="alt-services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row justify-content-around gy-4">
+          <div class="col-lg-6 img-bg" style="background-image: url(assets/img/alt-services.jpg);" data-aos="zoom-in" data-aos-delay="100"></div>
+
+          <div class="col-lg-5 d-flex flex-column justify-content-center">
+            <h2>WHAT WE PROVIDE</h2>
+           <P>The firm provides entire products range to the clients at very competitive and affordable prices and as well as ensures not to make any compromise with quality of raw materials used or for joints or for final paint finish of products. Therefore, the products find acceptance on top priority. Various different sections have been set up for different operations like metal sheet straightening, cutting, molding and shaping and for other different operations in stages of manufacturing, assembling and finishing. Quality Control is one major aspect that is taken care of very vigilantly so that there is no lapse in the quality that the firm intends to deliver. The close coordination of each and every workman in various sections and their cordial relationship creates an atmosphere of smooth functioning resulting in utmost productivity and growth of firm.</P>
+
+            <div class="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="100">
+              <i class="bi bi-easel flex-shrink-0"></i>
+              <div>
+                <h4><a href="" class="stretched-link">Lorem Ipsum</a></h4>
+                <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+              </div>
+            </div><!-- End Icon Box -->
+
+            <div class="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="200">
+              <i class="bi bi-patch-check flex-shrink-0"></i>
+              <div>
+                <h4><a href="" class="stretched-link">Nemo Enim</a></h4>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+              </div>
+            </div><!-- End Icon Box -->
+
+            <div class="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="300">
+              <i class="bi bi-brightness-high flex-shrink-0"></i>
+              <div>
+                <h4><a href="" class="stretched-link">Dine Pad</a></h4>
+                <p>Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
+              </div>
+            </div><!-- End Icon Box -->
+
+            <div class="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="400">
+              <i class="bi bi-brightness-high flex-shrink-0"></i>
+              <div>
+                <h4><a href="" class="stretched-link">Tride clov</a></h4>
+                <p>Est voluptatem labore deleniti quis a delectus et. Saepe dolorem libero sit non aspernatur odit amet. Et eligendi</p>
+              </div>
+            </div><!-- End Icon Box -->
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Alt Services Section -->
+
+  
+        
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="footer-content position-relative">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>HIGH TENSILE</h3>
+              <P>FASTNERS AND TOOLS</P>
+              <div class="social-links d-flex mt-3">
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
+              </div>
+            </div>
+          </div><!-- End footer info column-->
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Product</a></li>
+              <li><a href="#">Contact Us</a></li>
+             
+            </ul>
+          </div><!-- End footer links column-->
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><a href="#">Specialized in MOM and BOM boxes</a></li>
+              <li><a href="#"> manufacturing Electronic & Electrical cabinets</a></li>
+              <li><a href="#"> Sheet Metal Works</a></li>
+              <li><a href="#">Fire Boxs</a></li>
+              <li><a href="#">Metal Doors,windows,Frames & Honeycomb</a></li>
+              <li><a href="#">Cable Trays & perforations</a></li>
+              <li><a href="#">Clean Room Works</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h4>CONTACT US</h4>
+              <p>
+                4-6-67/1, Balanagar "X" Roads  <br>
+                Hyderabad - 500037 , Telangana<br><br>
+                <strong>Phone:</strong> +91 9885454346<br>
+                <strong>Phone:</strong> +91 7382090733<br>
+                <strong>Email:</strong> mufaddalkarodi@gmail.com<br>
+              </p>
+              <div class="social-links d-flex mt-3">
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
+              </div>
+            </div>
+        
+
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-legal text-center position-relative">
+      <div class="container">
+        <div class="copyright">
+          &copy; Copyright <strong><span>HIGH TENSILE</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+          
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+    </div>
+
+  </footer>
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <!-- ///nav linl here  -->
+<!-- Essential jQuery Plugins
+		================================================== -->
+	<!-- Main jQuery -->
+	<script src="plugins/jquery-1.11.1.min.js"></script>
+	<!-- Twitter Bootstrap -->
+	<script src="plugins/bootstrap/bootstrap.min.js"></script>
+	<!-- Single Page Nav -->
+	<script src="plugins/jquery.singlePageNav.min.js"></script>
+	<!-- jquery.fancybox.pack -->
+	<script src="plugins/jquery.fancybox.pack.js"></script>
+	<!-- Owl Carousel -->
+	<script src="plugins/owl.carousel.min.js"></script>
+	<!-- jquery easing -->
+	<script src="plugins/isotope.pkgd.min.js"></script>
+	<script src="plugins/jquery.easing.min.js"></script>
+	<!-- Fullscreen slider -->
+	<script src="plugins/jquery.slitslider.js"></script>
+	<script src="plugins/jquery.ba-cond.min.js"></script>
+	<!-- onscroll animation -->
+	<script src="plugins/wow.min.js"></script>
+	<!-- Custom Functions -->
+	<script src="plugins/jquery.form.min.js"></script>
+	<script src="plugins/jquery.validate.min.js"></script>
+	<!-- Google Map -->
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBI14J_PNWVd-m0gnUBkjmhoQyNyd7nllA"></script>
+	<script src="plugins/gmap.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
