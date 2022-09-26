3b2e4df2746a084885e5f13f04c96b58b696e0ff
DigitalNest101 <84118920+DigitalNest101@users.noreply.github.com>
2022-09-26 22:55:07 +0530

    Add files via upload

---
+++ partials/header.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<!-- meta character set -->
+	<meta charset="utf-8">
+	<!-- Always force latest IE rendering engine or request Chrome Frame -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>HIGH TENSILE</title>
+	<!-- Meta Description -->
+	<meta name="description" content="Blue One Page Creative HTML5 Template">
+	<meta name="keywords"
+		content="one page, single page, onepage, responsive, parallax, creative, business, html5, css3, css3 animation">
+	<meta name="author" content="Themefisher">
+
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- CSS
+		================================================== -->
+
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
+
+	<!-- Fontawesome Icon font -->
+	<link rel="stylesheet" href="./plugins/font-awesome.min.css">
+	<!-- Ion Icons Font -->
+	<link rel="stylesheet" href="./plugins/ionicons.min.css">
+	<!-- bootstrap.min -->
+	<link rel="stylesheet" href="./plugins/jquery.fancybox.css">
+	<!-- bootstrap.min -->
+	<link rel="stylesheet" href="./plugins/bootstrap/bootstrap.min.css">
+	<!-- bootstrap.min -->
+	<link rel="stylesheet" href="./plugins/owl.carousel.css">
+	<!-- bootstrap.min -->
+	<link rel="stylesheet" href="./plugins/slit-slider.css">
+	<!-- bootstrap.min -->
+	<link rel="stylesheet" href="./plugins/animate.css">
+	<!-- Main Stylesheet -->
+	<link rel="stylesheet" href="./css/style.css">
+	<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+	<!-- Modernizer Script for old Browsers -->
+	<script src="./plugins/modernizr-2.6.2.min.js"></script>
+
+	<!-- //constructioj here  -->
+	<!-- Vendor CSS Files -->
+	 <!-- <link href="./assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+  <!-- <link href="./assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">  -->
+  <!-- <link href="./assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">   -->
+  <!-- <link href="./assets/vendor/aos/aos.css" rel="stylesheet"> -->
+
+  <!-- Template Main CSS File -->
+  <!-- <link href="assets/css/main.css" rel="stylesheet"> -->
+
+
+
+
+
+</head>
+
+<body id="body">
+
+	<!-- preloader -->
+	<div id="preloader">
+		<div class="loder-box">
+			<div class="battery"></div>
+		</div>
+	</div>
+	<!-- end preloader -->
+
+	<!--
+        Fixed Navigation
+        ==================================== -->
+
+	<header id="navigation" class="fixed-top animated-header fa-x">
+		<div class="container ">
+			<nav class="navbar navbar-expand-lg navbar-dark ">
+				<div>
+				<a class="navbar-brand navlogo " href="#body">
+					<img src="./images/LOGO6.png" style="width:82px;height:32px; "class="img-fluid ">					
+					<span class="nav-title" style="font-size: 25PX; font-weight: 900; color:rgba(10, 166, 189, 0.75);">High Tensile</span>
+					<!-- <img src="/images/LOGO2.png" style="width:180px;height: 30px;" class=" img-fluid"> -->
+					<!-- <img src="/images/logo2.png" class="h-25 w-25 img-fluid"> -->
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse"
+					data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</div>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto text-center" id="nav">
+						
+							<a class="nav-link" href="./index.php">home</a>
+						
+						<li class="nav-item">
+							<a class="nav-link" href="./about.php">About</a>
+						</li>
+						<li class=" nav-item dropdown">
+							<a class="nav-link  dropdown-toggle " href="#" role="button" id="dropdownMenuLink"
+								data-mdb-toggle="dropdown" aria-expanded="false">
+								Product
+							</a>
+							<ul class="text-center navbar-dark bg-dark dropdown-menu drop"
+								aria-labelledby="dropdownMenuLink">
+
+								<li class="nav-item"><a class="nav-link " href="./services1.html">Specialized in MOM and BOM box
+										manufactuing</a></li>
+								<li class="nav-item"><a class="nav-link " href="/services2.html">Fire box</a></li>
+								<li class="nav-item"><a class="nav-link " href="#">Electronic & Electrical cabinets
+										manufactuing</a></li>
+								<li class="nav-item"><a class="nav-link " href="#">Cable Trays & perforations
+										manufactuing</a></li>
+								<li class="nav-item"><a class="nav-link " href="#">Clean Room Works
+										manufactuing</a></li>
+								<li class="nav-item"><a class="nav-link " href="#">Metal Doors,windows,Frames &
+										Honeycomb</a></li>
+
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="./contact.php">Contact US</a>
+						</li>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--
+              End Fixed Navigation
+              ==================================== -->
